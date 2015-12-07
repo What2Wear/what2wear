d9fcfbeee4731a3378a39d22d17096b1abea9206
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-12-07 11:30:07 -0600

    refactoring code

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("#sign_up").click(function(e){
+    e.preventDefault();
+    var data = {};
+
+    data["username"] = $("#username").val();
+    data["phone"] = $("#phone").val();
+    data["password"] = $("#password").val();
+    data["gender"] = $("#gender").val();
+
+    $.post("/addData", data);
+    window.location = "/";
+  });
+
+  $("#sign_in")
+});
