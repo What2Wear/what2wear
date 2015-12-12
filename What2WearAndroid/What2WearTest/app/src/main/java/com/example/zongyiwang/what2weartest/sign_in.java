99042a3e01cca30036db363e076adf551658ced5
Ben2356 <BenM2356@gmail.com>
2015-12-12 16:30:05 -0600

    moar

--- What2WearAndroid/What2WearTest/app/src/main/java/com/example/zongyiwang/what2weartest/sign_in.java
+++ What2WearAndroid/What2WearTest/app/src/main/java/com/example/zongyiwang/what2weartest/sign_in.java
@@ -1,8 +1,10 @@
 package com.example.zongyiwang.what2weartest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by Ben25 on 12/8/2015.
@@ -14,5 +16,12 @@ public class sign_in extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    public void toProfilePage(View view)
+    {
+        Intent profilePage = getIntent();
+        profilePage = new Intent(this,ProfileinfoActivity.class);
+        startActivity(profilePage);
     }
 }
