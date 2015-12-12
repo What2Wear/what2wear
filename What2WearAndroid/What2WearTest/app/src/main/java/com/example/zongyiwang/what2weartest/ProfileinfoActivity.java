098d3881942bcb4929e8c380d5ea9329013f36e0
Ben2356 <BenM2356@gmail.com>
2015-12-12 16:55:39 -0600

    linking

--- What2WearAndroid/What2WearTest/app/src/main/java/com/example/zongyiwang/what2weartest/ProfileinfoActivity.java
+++ What2WearAndroid/What2WearTest/app/src/main/java/com/example/zongyiwang/what2weartest/ProfileinfoActivity.java
@@ -1,10 +1,9 @@
 package com.example.zongyiwang.what2weartest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 
 public class ProfileinfoActivity extends AppCompatActivity {
@@ -13,13 +12,18 @@ public class ProfileinfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profileinfo);
-
-        final Button button = (Button) findViewById(R.id.button_what2wear);
     }
 
     public void GetWhat2Wear(View view) {
         Intent what2wear = getIntent();
         what2wear = new Intent(this,what2wear_weather.class);
         startActivity(what2wear);
+    }
+
+    public void toChangeProfile(View view)
+    {
+        Intent changeProfile = getIntent();
+        changeProfile = new Intent(this,change_profile.class);
+        startActivity(changeProfile);
     }
 }
--- What2WearAndroid/What2WearTest/app/src/main/java/com/example/zongyiwang/what2weartest/change_profile.java
+++ What2WearAndroid/What2WearTest/app/src/main/java/com/example/zongyiwang/what2weartest/change_profile.java
@@ -1,7 +1,26 @@
 package com.example.zongyiwang.what2weartest;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by zongyiwang on 12/12/15.
  */
-public class change_profile {
+public class change_profile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile);
+    }
+
+    public void changeToProfile(View view)
+    {
+        Intent toProfile = getIntent();
+        toProfile = new Intent(this,ProfileinfoActivity.class);
+        startActivity(toProfile);
+    }
 }
+
+
