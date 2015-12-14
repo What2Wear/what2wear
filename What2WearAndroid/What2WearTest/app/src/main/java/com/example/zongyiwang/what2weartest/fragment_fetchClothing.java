a1ef37ce975bb28273e16fa5445d4c87a7afa079
Ben2356 <BenM2356@gmail.com>
2015-12-13 19:21:32 -0600

    running out of names for these commits

--- What2WearAndroid/What2WearTest/app/src/main/java/com/example/zongyiwang/what2weartest/fragment_fetchClothing.java
+++ What2WearAndroid/What2WearTest/app/src/main/java/com/example/zongyiwang/what2weartest/fragment_fetchClothing.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class fragment_fetchClothing extends Fragment {
@@ -12,7 +14,13 @@ public class fragment_fetchClothing extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_fetch_clothing, container, false);
-    }
+        ImageView imageView = (ImageView) getView().findViewById(R.id.tempImage);
+        TextView temp = (TextView)getView().findViewById(R.id.fragment_temp);
+        TextView what2wear = (TextView)getView().findViewById(R.id.fragmet_w2w);
 
+        
+
+        return inflater.inflate(R.layout.fragment_fragment_fetch_clothing, container, false);
+
+    }
 }
